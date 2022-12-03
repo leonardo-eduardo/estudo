ef06f71d6860746479dab6d7dbd3a9c77af8d4f6
leonardo.eduardo <leonardo.eduardo@sensedia.com>
2022-12-03 16:25:34 -0300

    Insercão do parâmetro CPF

--- Application.java
+++ Application.java
@@ -2,6 +2,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Nome: Leonardo dos Santos Eduardo");
         System.out.println("CPF: 078.531.417-26");
-
+        System.out.println("Sexo: Masculino");
     }
 }
