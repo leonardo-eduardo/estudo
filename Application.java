4482e770b11364e87ee9c454ed90ba1a661f09d8
leonardo.eduardo <leonardo.eduardo@sensedia.com>
2022-12-04 08:02:09 -0300

    commit 1

---
+++ Application.java
@@ -0,0 +1,8 @@
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Nome: Leonardo dos Santos Eduardo");
+        System.out.println("CPF: 078.531.417-26");
+        System.out.println("Sexo: Masculino");
+        System.out.println("Tipo de Pessoa: Física");
+    }
+}
