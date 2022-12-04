d780fafbb635d6e4ec5b7998ceb0c7536872eb72
leonardo.eduardo <leonardo.eduardo@sensedia.com>
2022-12-04 07:10:19 -0300

    exclusão de parâmetros

--- Application.java
+++ Application.java
@@ -4,10 +4,7 @@ public class Application {
         System.out.println("CPF: 078.531.417-26");
         System.out.println("Sexo: Masculino");
         System.out.println("Tipo de Pessoa: Física");
-        System.out.println("Tipo de Pessoa: Física");
-        System.out.println("Tipo de Pessoa: Física");
-        System.out.println("Tipo de Pessoa: Física");
-        System.out.println("Tipo de Pessoa: Física");
+
 
 
     }
